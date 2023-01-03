a6692093d2c1faf1bc05c39c54bf70547f1e6e0e
Jordan Rust <jordan@oxidalwave.com>
2023-01-03 16:52:25 -0600

    Use SECRET envvar

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,18 +1,27 @@
 import NextAuth from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import DiscordProvider, { DiscordProfile } from "next-auth/providers/discord";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "lib/prisma";
+import { OAuthConfig } from "next-auth/providers";
+
+const discord: OAuthConfig<DiscordProfile>[] =
+  process.env.DISCORD_CLIENT_ID && process.env.DISCORD_CLIENT_SECRET
+    ? [
+        DiscordProvider({
+          clientId: process.env.DISCORD_CLIENT_ID,
+          clientSecret: process.env.DISCORD_CLIENT_SECRET,
+        }),
+      ]
+    : [];
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID ?? '',
-      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
-    }),
+    ...discord,
     // ...add more providers here
   ],
+  secret: process.env.SECRET,
 };
 
 export default NextAuth(authOptions);
