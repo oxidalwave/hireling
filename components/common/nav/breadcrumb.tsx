870a7966585cf659d34e58639218b4418df80743
Jordan Rust <jordan@oxidalwave.com>
2023-01-04 13:58:53 -0600

    Use Next Links for breadcrumbs

--- components/common/nav/breadcrumb.tsx
+++ components/common/nav/breadcrumb.tsx
@@ -1,4 +1,5 @@
 import { Breadcrumbs, Anchor } from "@mantine/core";
+import Link from "next/link";
 
 export default function Breadcrumb({ path }) {
   const pathParts = path
@@ -11,13 +12,18 @@ export default function Breadcrumb({ path }) {
 
   return (
     <Breadcrumbs>
-      <Anchor href="/">Home</Anchor>
+      <Link legacyBehavior href="/">
+        <Anchor>Home</Anchor>
+      </Link>
+
       {pathParts.map((pathPart, i) => (
-        <Anchor href={pathPart.href} key={`breadcrumb-${i}`}>
-          {pathPart.label
-            .split(" ")
-            .map((w, i) => w[0].toUpperCase() + w.substring(1))}
-        </Anchor>
+        <Link key={`breadcrumb-${i}`} legacyBehavior href={pathPart.href}>
+          <Anchor>
+            {pathPart.label
+              .split(" ")
+              .map((w, i) => w[0].toUpperCase() + w.substring(1))}
+          </Anchor>
+        </Link>
       ))}
     </Breadcrumbs>
   );
